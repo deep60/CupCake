48732128679bd86250c279c7fe1f7ceaf06edd43
P Deepanshu <97436890+deep60@users.noreply.github.com>
2022-12-31 08:23:26 +0530

    Add files via upload

---
+++ CupcakeCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCenterApp.swift
+//  CupcakeCenter
+//
+//  Created by P Deepanshu on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
